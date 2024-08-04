714f8f088dc8ad499cdf20812dead627e3b692ce
hetbhalani <bhalanihet2006@gmail.com>
2024-08-04 11:59:11 +0530

    add button

---
+++ app.js
@@ -0,0 +1,13 @@
+
+document.querySelector('.auto').addEventListener('click', function() {
+
+        document.querySelector('.manual').style.backgroundColor = '#071d2a';
+        document.querySelector('.auto').style.backgroundColor = '#2f4553';
+});
+
+document.querySelector('.manual').addEventListener('click', function() {
+
+        document.querySelector('.manual').style.backgroundColor = '#2f4553';
+        document.querySelector('.auto').style.backgroundColor = '#071d2a';
+    
+});
